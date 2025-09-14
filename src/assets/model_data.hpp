847ed3ad454aadbad14465918b07a8ea2c0da3a8
nesktf <nesktf@proton.me>
2025-09-13 21:03:03 -0300

    Move thing to model_data

--- src/assets/model_data.hpp
+++ src/assets/model_data.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../common.hpp"
+#include "../renderer/context.hpp"
 
 #include <ntfstl/unique_array.hpp>
 #include <ntfstl/optional.hpp>
@@ -169,6 +170,219 @@ private:
   Assimp::Importer _imp;
   uint32 _flags;
   std::string _dir;
+};
+
+struct vertex_config {
+  size_t size;
+  string_view name;
+};
+
+namespace meta {
+
+template<typename Arr, typename T>
+struct is_std_array_of : public std::false_type{};
+
+template<typename T, size_t N>
+struct is_std_array_of<std::array<T, N>, T> : public std::true_type{};
+
+template<typename T>
+concept mesh_data_type = requires(const T mesh_data, size_t idx) {
+  requires std::convertible_to<decltype(T::USE_INDICES), bool>;
+  requires ntf::meta::std_cont<std::remove_const_t<decltype(T::VERT_CONFIG)>, vertex_config>;
+  { mesh_data.vertex_count() } -> std::convertible_to<size_t>;
+  { mesh_data.index_count() } -> std::convertible_to<size_t>;
+  { mesh_data.get_data_ptr(idx) } -> std::same_as<std::pair<const void*, size_t>>;
+};
+
+} // namespace meta
+
+
+template<meta::mesh_data_type MeshDataT>
+class mesh_buffers {
+private:
+  static constexpr size_t ATTRIB_COUNT =
+    MeshDataT::VERT_CONFIG.size() + static_cast<size_t>(MeshDataT::USE_INDICES);
+  static constexpr size_t INDICES_BUFFER_IDX = MeshDataT::VERT_CONFIG.size()+1u;
+
+  struct mesh_offset {
+    u32 index_offset;
+    u32 index_count;
+    u32 vertex_offset;
+  };
+
+public:
+  using vert_buffs = std::array<shogle::buffer_t, ATTRIB_COUNT>;
+  using vert_binds = std::array<shogle::vertex_binding, ATTRIB_COUNT>;
+
+public:
+  mesh_buffers(vert_buffs buffs, ntf::unique_array<mesh_offset>&& offsets) :
+    _buffs{buffs}, _offsets{std::move(offsets)}
+  {
+    for (u32 i = 0; i < _buffs.size(); ++i) {
+      NTF_ASSERT(_buffs[i], "Vertex \"{}\" missing", MeshDataT::VERT_CONF[i].name);
+      _binds[i].buffer = _buffs[i];
+      _binds[i].layout = i;
+    }
+  }
+
+  mesh_buffers(mesh_buffers&& other) noexcept :
+    _buffs{std::move(other._buffs)}, _binds{std::move(other._binds)},
+    _offsets{std::move(other._offsets)}
+  {
+    other._reset_buffs();
+  }
+
+  ~mesh_buffers() noexcept { _free_buffs(); }
+  mesh_buffers(const mesh_buffers&) = delete;
+
+public:
+  mesh_buffers& operator=(mesh_buffers&& other) noexcept {
+    _free_buffs();
+    
+    _buffs = std::move(other._buffs);
+    _binds = std::move(other._binds);
+    _offsets = std::move(other._offsets);
+
+    other._reset_buffs();
+    return *this;
+  }
+
+  mesh_buffers& operator=(const mesh_buffers&) = delete;
+
+private:
+  void _reset_buffs() noexcept {
+    std::memset(_buffs.data(), 0, _buffs.size()*sizeof(shogle::buffer_t));
+    std::memset(_binds.data(), 0, _binds.data()*sizeof(shogle::vertex_binding));
+  }
+
+  void _free_buffs() noexcept {
+    if (!_buffs[0]) {
+      return;
+    }
+    for (shogle::buffer_t buff : _buffs) {
+      if (buff) {
+        shogle::destroy_buffer(buff);
+      }
+    }
+  }
+
+public:
+  static expect<mesh_buffers> create(const MeshDataT& mesh_data) {
+    auto ctx = renderer::instance().ctx();
+    vert_buffs buffs{}; // init as nullptr
+
+    auto free_buffs = [&]() {
+      for (shogle::buffer_t buff : buffs) {
+        if (buff) {
+          shogle::destroy_buffer(buff);
+        }
+      }
+    };
+
+    for (u32 i = 0; const auto& [conf_size, conf_name] : MeshDataT::VERT_CONFIG) {
+      ntf::logger::debug("Creating vertex \"{}\"", conf_name);
+      auto buff =  shogle::create_buffer(ctx, {
+        .type = shogle::buffer_type::vertex,
+        .flags = shogle::buffer_flag::dynamic_storage,
+        .size = mesh_data.vertex_count()*conf_size,
+        .data = nullptr,
+      });
+      if (!buff) {
+        free_buffs();
+        return {ntf::unexpect, buff.error()};
+      }
+      buffs[i] = *buff;
+      ++i;
+    }
+    constexpr size_t idx_buff_pos = ATTRIB_COUNT-1;
+    if constexpr (MeshDataT::USE_INDICES) {
+      ntf::logger::debug("Creating index buffer");
+      const size_t idx_count = mesh_data.index_count();
+      auto ind = shogle::create_buffer(ctx, {
+        .type = shogle::buffer_type::vertex,
+        .flags = shogle::buffer_flag::dynamic_storage,
+        .size = idx_count*sizeof(u32),
+        .data = nullptr,
+      });
+      if (!ind) {
+        free_buffs();
+        return {ntf::unexpect, ind.error()};
+      }
+      buffs[idx_buff_pos] = *ind;
+    }
+
+
+    // Copy data
+    size_t offset = 0u;
+    const size_t mesh_count = mesh_data.mesh_count();
+    ntf::unique_array<mesh_offset> mesh_offsets(mesh_count);
+    for (size_t mesh_idx = 0; mesh_idx < mesh_count; ++mesh_idx) {
+
+      if constexpr (MeshDataT::USE_INDICES) {
+        const auto idx_vspan = mesh_data.idx_vspan_at(mesh_idx);
+        mesh_offsets[mesh_idx].index_offset = idx_vspan.idx;
+        mesh_offsets[mesh_idx].index_count = idx_vspan.count;
+      } else {
+        mesh_offsets[mesh_idx].index_offset = 0u;
+        mesh_offsets[mesh_idx].index_count = 0u;
+      }
+
+      mesh_offsets[mesh_idx].vertex_offset = offset;
+
+      size_t advance = 0u;
+      for (u32 buff_idx = 0; const auto& [conf_size, conf_name] : MeshDataT::VERT_CONFIG) {
+        ntf::logger::debug("Uploading vertex data \"{}\"", conf_name);
+        const auto [data_ptr, data_count] = mesh_data.get_data_ptr(buff_idx);
+        if (!data_ptr) {
+          ntf::logger::warning("Vertex with \"{}\" with no data", conf_name);
+          continue;
+        }
+        advance = std::max(advance, data_count);
+        NTF_ASSERT(buffs[buff_idx]);
+        [[maybe_unused]] auto ret = shogle::buffer_upload(buffs[buff_idx], {
+          .data = data_ptr,
+          .size = data_count*conf_size,
+          .offset = offset*conf_size,
+        });
+        ++buff_idx;
+      }
+      offset += advance;
+    }
+    if constexpr (MeshDataT::USE_INDICES) {
+      ntf::logger::debug("Uploading index data");
+      const auto [data_ptr, data_count] = mesh_data.get_index_data();
+      if (!data_ptr) {
+        ntf::logger::warning("No index data");
+      } else {
+        NTF_ASSERT(buffs[idx_buff_pos]);
+        [[maybe_unused]] auto ret = shogle::buffer_upload(buffs[idx_buff_pos], {
+          .data = data_ptr,
+          .size = data_count*sizeof(u32),
+          .offset = offset*sizeof(u32),
+        });
+      }
+    }
+    return {ntf::in_place, buffs, std::move(mesh_offsets)};
+  }
+
+public:
+  mesh_render_data& retrieve_meshes(u32 idx, std::vector<mesh_render_data>& data) {
+    if constexpr (MeshDataT::USE_INDICES) {
+      NTF_ASSERT(idx < _offsets.size()-1);
+    } else {
+      NTF_ASSERT(idx < _offsets.size());
+    }
+    cspan<shogle::vertex_binding> binds{_binds.data(), _binds.size()};
+    const auto& offset = _offsets[idx];
+    shogle::buffer_view idx_buff{_binds[_binds.size()-1]};
+    return data.emplace_back(binds, shogle::to_typed<shogle::buffer_type::index>(idx_buff),
+                             offset.index_count, offset.vertex_count, offset.index_count, 0u);
+  }
+
+private:
+  vert_buffs _buffs;
+  vert_binds _binds;
+  ntf::unique_array<mesh_offset> _offsets;
 };
 
 } // namespace kappa
